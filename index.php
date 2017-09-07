34c4b258e6607463ba15626764bf16c124731526
cyberplanet <chonlapat_2004@hotmail.com>
2017-09-07 22:25:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'I am a IBMSD'
+				'text' => 'I am a BOT'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -56,7 +56,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'I am a IBMSD'
+				'text' => 'I am a BOT'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
